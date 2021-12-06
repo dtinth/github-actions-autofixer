c8c356c54d3f359e17f5695623988e671a212277
dtinth on MBP M1 <dtinth@spacet.me>
2021-12-06 15:50:16 +0700

    Clean up for now

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,7 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  expect(document.body.textContent).toMatchInlineSnapshot(`undefined`)
-});
+  expect(document.body.textContent).toMatchInlineSnapshot(
+    `"Edit src/App.js and save to reload.Learn React"`,
+  )
+})
