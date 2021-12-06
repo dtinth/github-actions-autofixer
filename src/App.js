e266d5bd052cfeec5f26e7b8c47e292f45a878e3
dtinth on MBP M1 <dtinth@spacet.me>
2021-12-06 15:51:36 +0700

    Modify some stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello world! I'm a React App.</p>
+        <p>Hello world! I'm a React App. This app doesn't do anything useful; it's just a demo.</p>
       </header>
     </div>
   )
